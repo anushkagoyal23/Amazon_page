c4d14b36cd9bb8c0fdb110c0c3e5ae194052dbe0
anushkagoyal23 <anushkagoyal534@gmail.com>
2024-06-12 16:56:25 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addToCartBtn = document.getElementById('addToCartBtn');
+    
+    addToCartBtn.addEventListener('click', function() {
+        alert('The product has been added to your cart!');
+    });
+});
